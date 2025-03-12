1bdd45c7189935d2c1a7ec10044df1d1ff58e65a
NidwijaB <nidwijab@gmail.com>
2025-03-12 10:03:23 +0545

    jeeveapp

--- test_cases/test_jeeve.py
+++ test_cases/test_jeeve.py
@@ -1,8 +1,11 @@
 # import logging
 import logging
+import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 from setup.basetest import BaseTest
 
@@ -14,3 +17,11 @@ class TestLogs(BaseTest):
         logging.info("opened the site")
         title = self.driver.title
         logging.info(title)
+        hover_element=self.driver.find_element(By.XPATH, "//div[text()='My Account']")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(hover_element).perform()
+        logging.info("opened dropdown")
+        login_btn=self.driver.find_element(By.XPATH, "//div[text()='Login']")
+        login_btn.click()
+        logging.info("opened the dropdown and clicked on login")
+        time.sleep(6)
